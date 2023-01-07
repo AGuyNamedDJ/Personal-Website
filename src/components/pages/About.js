3ce6d06dc0c94a2c020339620a11355cdd6f87b3
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-07 16:01:47 -0600

    updated front page, almost satisfied

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,9 +7,28 @@ const About = () => {
             <div id="about">
                 <h2 id="about-text">Dalron J.</h2>
                 <img src="/images/djr.jpg" alt="dj" id="about-page-image"></img>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
             </div>
             <footer id="footer">
-                <p id="footer-text">2023 Dalron J. Robertson. All Rights Reserved</p>
+                <p id="footer-top-text">Stay connected with me here.</p>
+                <p id="footer-top-text">SUBMIT</p>
+                
+            </footer>
+            <br></br>
+            
+            <footer id="footer-one-container">
+                <div>
+                    2023 Dalron J. Robertson.  All Rights Reserved.
+                </div>
+                <div id="footer-websites-container">
+                    <p class="footer-websites">CONTACT</p>
+                    <p class="footer-websites">GITHUB</p>
+                    <p class="footer-websites">INSTAGRAM</p>
+                    <p class="footer-websites">LINKEDIN</p>
+                </div>
             </footer>
         </div>
 
