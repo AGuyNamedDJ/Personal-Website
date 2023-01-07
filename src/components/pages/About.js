bb1a835650b1c629c18b0d7c0ec5b9d235327718
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-07 13:02:08 -0600

    updates 2

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,16 @@ import React from "react";
 const About = () => {
 
     return (
-        <div id="about">
-            <h2 id="about-text">Dalron J.</h2>
+        <div>
+            <div id="about">
+                <h2 id="about-text">Dalron J.</h2>
+                <img src="/images/djr.jpg" alt="dj" id="about-page-image"></img>
+            </div>
+            <footer id="footer">
+                <p id="footer-text">Copyright 2023 My Website</p>
+            </footer>
         </div>
+
     )
 };
 
--- src/components/utilities/ErrorPage.js
+++ src/components/utilities/ErrorPage.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 const ErrorPage = () => {
 
     return (
         <div id="error-page">
-            <h2 id="error-page-text">404 Error, please try again.</h2>
-        
+            <h2 id="error-page-text">Uh ohh, Peter hurt himself looking for your page..</h2>
+            <img src="/images/peterg.webp" alt="peterg" id="error-page-image"/>
         </div>
     )
 };
