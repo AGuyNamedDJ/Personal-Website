f695b68f11bd7aa54a2ecad4a666adf8942ee210
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-10 16:27:38 -0600

    journal entry complete

--- src/components/pages/JournalRejection.js
+++ src/components/pages/JournalRejection.js
@@ -5,10 +5,23 @@ const JournalRejection = () => {
 
     return (
         <div>
+            {/* Job Search Rejection */}
             <div class="title">Job Search Rejection</div>
             <br></br>
             <br></br>
+            <p id="journal-date-detail">`Jan 10, 2022 &#x2023; 2 min`</p>
 
+            {/* Image */}
+            <img src="/images/journal-1.jpeg" id="journal-rejection-detail"></img>
+            <br></br>
+            <br></br>
+
+            <p id="journal-text-detail">Since Graduating from one of the Top Bootcamps over a month ago, I have submitted over 200 applications to entry and associate software engineering roles. Every morning, my emails are flooded with rejection notices.</p>
+            <p id="journal-text-detail">To all of the recent grads, I'm here with you. I've never felt this in my previous field. I've never been consistently rejected before. I've always gotten the interview. But for now I'll keep trying.</p>
+            <p id="journal-text-detail">I've also decided to make a new full stack website every 4-5 days. I'm going to keep adding to my tech stack. And I'm going to make it! God brought me to tech so I know I'll get the role I want soon.</p>
+
+            <br></br>
+            <br></br>
 
             {/* End of Page */}
             <footer id="footer">
