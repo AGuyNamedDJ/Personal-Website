5a44ed70d6cd40d4c6db1cb96da0aea9f1ea7378
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-08 11:12:19 -0500

    Complete redo

--- src/index.js
+++ src/index.js
@@ -8,20 +8,8 @@ import ErrorPage from "./components//utilities/ErrorPage";
 import Index from "./components/utilities/Index";
 
 // Imports-Pages
-import About from "./components/pages/About";
-import Journal from "./components/pages/Journal";
-import JournalAI from "./components/pages/JournalAI";
-import JournalRejection from "./components/pages/JournalRejection";
-import Memoirs from "./components/pages/Memoirs";
-import MemoirsChicago from "./components/pages/MemoirsChicago";
-import MemoirsMontegoBay from "./components/pages/MemoirsMontegoBay";
-import MemoirsQuintanaRoo from "./components/pages/MemoirsQuintanaRoo";
-import Photography from "./components/pages/Photography";
-import Work from "./components/pages/Work";
-import WorkCV from "./components/pages/WorkCV";
-import WorkResume from "./components/pages/WorkResume";
-import WorkWebsites from "./components/pages/WorkWebsites";
 
+// BrowserRouter
 const router = createBrowserRouter([
     {
         path: "/",
@@ -31,54 +19,6 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <About />
-            },
-            {
-                path: "/journal",
-                element: <Journal />
-            },
-            {
-                path: "/journal/ai",
-                element: <JournalAI />
-            },
-            {
-                path: "/journal/rejection",
-                element: <JournalRejection />
-            },
-            {
-                path: "/memoirs",
-                element: <Memoirs />
-            },
-            {
-                path: "/memoirs/chicago",
-                element: <MemoirsChicago />
-            },
-            {
-                path: "/memoirs/montegobay",
-                element: <MemoirsMontegoBay />
-            },
-            {
-                path: "/memoirs/quintanaroo",
-                element: <MemoirsQuintanaRoo />
-            },
-            {
-                path: "/photography",
-                element: <Photography />
-            },
-            {
-                path: "/work",
-                element: <Work />
-            },
-            {
-                path: "/work/cv",
-                element: <WorkCV />
-            },
-            {
-                path: "/work/resume",
-                element: <WorkResume />
-            },
-            {
-                path: "/work/websites",
-                element: <WorkWebsites />
             }
         ]
     }
