4b244d18119b39385313bf2c2d10ff725630f622
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-08 12:18:55 -0500

    update

---
+++ src/components/pages/HomepageContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+const HomepageContent = () => {
+
+    return (
+        <div id="Homepage-content">
+            <p>HomePage</p>
+        </div>
+    )
+};
+
+export default HomepageContent;
--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -5,17 +5,16 @@ import { Link } from "react-router-dom";
 const Navbar = ({context}) => {
     return (
         <nav id="navbar">
-            <Link to="/" id="navbar-text">ABOUT</Link>
-            <Link to="/memoirs" id="navbar-text">MEMOIRS</Link>
-            <Link to="/photography" id="navbar-text">PHOTOGRAPHY</Link>
-            <Link to="/work" id="navbar-text">WORK</Link>
-            <Link to="/journal" id="navbar-text">JOURNAL</Link>
+            <Link to="/" id="navbar-text">Home</Link>
+            <Link to="/about" id="navbar-text">ABOUT</Link>
+            <Link to="/projects" id="navbar-text">PROJECTS</Link>
+            <Link to="/ventures" id="navbar-text">VENTURES</Link>
+            <Link to="/credentials" id="navbar-text">CREDENTIALS</Link>
+            <Link to="/connect" id="navbar-text">CONNECT</Link>
         </nav>
     )
 };
 
 export default Navbar;
 
-// Notes
-    // the homepage will be the about page
-    // i want a drop down for the about page
+
