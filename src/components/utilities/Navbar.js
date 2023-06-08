8ab41cf11bb3db6001274f5103997f30da5bce1e
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-08 14:25:26 -0500

    Beginning to like style

--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = ({context}) => {
     return (
         <nav id="navbar">
             <Link to="/" id="navbar-home"><img src="/images/AGNDJ.png" alt="Home"/></Link>
+            <Link to="/about" id="navbar-text">ABOUT</Link>
             <Link to="/projects" id="navbar-text">PROJECTS</Link>
             <Link to="/ventures" id="navbar-text">VENTURES</Link>
             <Link to="/credentials" id="navbar-text">CREDENTIALS</Link>
