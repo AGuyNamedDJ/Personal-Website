021fa3d1ed03bfe3822623d5f5b9fe9d3ce1b244
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-09 10:47:08 -0500

    Text updates

--- src/components/pages/Ventures.js
+++ src/components/pages/Ventures.js
@@ -6,6 +6,14 @@ const Ventures = () => {
             <div id="ventures-1">
                 <p className ="ventures-1-title">Ventures</p>
             </div>
+            <div id="ventures-2" className="ventures-2-container">
+                <div className="ventures-2-text">
+                    <p>I am deeply committed to serving as a powerful catalyst for transformative change within underrepresented communities. With interests spanning real estate, technology, logistics, life sciences, retail and service sectors., my passion is not defined by the pursuit of wealth but by the relentless drive to spur economic vitality. My objective is to invigorate local economies, catalyze financial circulation, and fundamentally uplift communities. I am here to unlock opportunities, to debunk impossibilities, and to inspire learning, growth, and prosperity. Aspirations of wealth are secondary to the success of the communities I serve, for their triumph is our collective victory. This is a long-term mission - a journey for profound change. </p>
+                </div>
+                <div className="ventures-2-image">
+                    <img src="/images/chicago-skyline.jpg" alt="Descriptive Image Text" />
+                </div>
+            </div>
         </div>
     )
 };
