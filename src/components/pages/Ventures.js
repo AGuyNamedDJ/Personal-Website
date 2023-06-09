23b3944a07437cf8dcfb92115ac5728313ee34ae
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-09 11:14:28 -0500

    1st section almost complete on venture

--- src/components/pages/Ventures.js
+++ src/components/pages/Ventures.js
@@ -6,9 +6,11 @@ const Ventures = () => {
             <div id="ventures-1">
                 <p className ="ventures-1-title">Ventures</p>
             </div>
+
             <div id="ventures-2" className="ventures-2-container">
                 <div className="ventures-2-text">
-                    <p>I am deeply committed to serving as a powerful catalyst for transformative change within underrepresented communities. With interests spanning real estate, technology, logistics, life sciences, retail and service sectors., my passion is not defined by the pursuit of wealth but by the relentless drive to spur economic vitality. My objective is to invigorate local economies, catalyze financial circulation, and fundamentally uplift communities. I am here to unlock opportunities, to debunk impossibilities, and to inspire learning, growth, and prosperity. Aspirations of wealth are secondary to the success of the communities I serve, for their triumph is our collective victory. This is a long-term mission - a journey for profound change. </p>
+                    <h2 class="ventures-2-title">This is my<br /><span>Purpose<span class="dot"></span></span></h2>
+                    <p>I am deeply committed to serving as a powerful catalyst for transformative change within underrepresented communities. With interests spanning real estate, technology, life sciences, retail and service sectors., my passion is not defined by the pursuit of wealth rather the relentless drive to spur growth. My objective is to invigorate, catalyze, and uplift. I am here to unlock opportunities, to debunk impossibilities, and to inspire. Aspirations of wealth are secondary to the success of those I serve. This is a long-term mission - a journey for profound change.</p>
                 </div>
                 <div className="ventures-2-image">
                     <img src="/images/chicago-skyline.jpg" alt="Descriptive Image Text" />
