c54534f002492f0cf8e126109340b0af1e9d5e97
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-09 09:58:43 -0500

    Font

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,14 +2,13 @@ import React from "react";
 const HomePage = () => {
 
     return (
-        <div id="homepage">
+        <div className="homepage">
             <div id="homepage-1">
-                <p id="homepage-maintaince">Undergoing Maintenance</p>
-                <p id="homepage-maintaince">Undergoing Maintenance</p>
+                <p id="homepage-maintaince">Currently Undergoing Maintenance</p>
             </div>
             <div id="homepage-2">
-                <p id="homepage-maintaince">Undergoing Maintenance</p>
-                <p id="homepage-maintaince">Undergoing Maintenance</p>
+                {/* <p id="homepage-maintaince">Undergoing Maintenance</p>
+                <p id="homepage-maintaince">Undergoing Maintenance</p> */}
             </div>
         </div>
     )
--- src/components/pages/Ventures.js
+++ src/components/pages/Ventures.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Ventures = () => {
     return(
-        <div>
-            <p>Here is your default</p>
+        <div className="ventures">
+            <div id="ventures-1">
+                <p className ="ventures-1-title">Ventures</p>
+            </div>
         </div>
     )
 };
