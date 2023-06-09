56a39b27bd49c861b368952ed471ad05726fb833
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-09 11:27:18 -0500

    Improvement

--- src/components/pages/Ventures.js
+++ src/components/pages/Ventures.js
@@ -9,8 +9,8 @@ const Ventures = () => {
 
             <div id="ventures-2" className="ventures-2-container">
                 <div className="ventures-2-text">
-                    <h2 class="ventures-2-title">This is my<br /><span>Purpose<span class="dot"></span></span></h2>
-                    <p>I am deeply committed to serving as a powerful catalyst for transformative change within underrepresented communities. With interests spanning real estate, technology, life sciences, retail and service sectors., my passion is not defined by the pursuit of wealth rather the relentless drive to spur growth. My objective is to invigorate, catalyze, and uplift. I am here to unlock opportunities, to debunk impossibilities, and to inspire. Aspirations of wealth are secondary to the success of those I serve. This is a long-term mission - a journey for profound change.</p>
+                    <h2 className="ventures-2-title">This is my<br /><span>Purpose<span class="dot"></span></span></h2>
+                    <p>I am deeply committed to serving as a powerful catalyst for metamorphic change within underrepresented communities. With interests spanning real estate, technology, life sciences, retail and service sectors., my passion is not defined by the pursuit of wealth rather the relentless drive to spur change. My objective is to invigorate, catalyze, and uplift. I am here to unlock opportunities, to debunk impossibilities, and to inspire. Aspirations of wealth are secondary to the success of those I serve. This is a long-term mission - a journey for profound advancement.</p>
                 </div>
                 <div className="ventures-2-image">
                     <img src="/images/chicago-skyline.jpg" alt="Descriptive Image Text" />
