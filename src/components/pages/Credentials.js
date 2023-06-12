d0f542f787bafec43d1eef55d8f6cada09ff9aa5
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-11 22:23:45 -0500

    Updated

--- src/components/pages/Credentials.js
+++ src/components/pages/Credentials.js
@@ -21,15 +21,25 @@ const Credentials = () => {
             <div id="cvSection" className="section-credentials" style={{textAlign: "center"}}>
                 <h1>Curriculum Vitae</h1>
                 <div className="buttonContainer">
-                    <a href="https://www.dropbox.com/s/pdwtfs8ymntvl1s/DJR-Curriculum%20Vitae.docx-2.pdf?dl=0" target="_blank" rel="noopener noreferrer" className="cvButton">View CV</a>
-                    <a href="https://www.dropbox.com/s/pdwtfs8ymntvl1s/DJR-Curriculum%20Vitae.docx-2.pdf?dl=1" download className="cvButton">Download CV</a>
+                    <a href="https://www.dropbox.com/s/pdwtfs8ymntvl1s/DJR-Curriculum%20Vitae.docx-2.pdf?dl=0" target="_blank" rel="noopener noreferrer" className="cvButton">View Curriculum Vitae</a>
+                    <a href="https://www.dropbox.com/s/pdwtfs8ymntvl1s/DJR-Curriculum%20Vitae.docx-2.pdf?dl=1" download className="cvButton">Download Curriculum Vitae</a>
+                </div>
+            </div>
+
+            {/* Certifications Section */}
+            <div id="certificatesSection" className="section-credentials" style={{textAlign: "center"}}>
+                <h1>Certifications</h1>
+                <div className="buttonContainer">
+                    <a href="https://www.dropbox.com/sh/tqxl9vgpobnnvso/AAD_nK8q8oY3e0a0IH-dZIHca?dl=0" target="_blank" rel="noopener noreferrer" className="certificationButton">View Certifications</a>
                 </div>
             </div>
     
             {/* Notoriety Section */}
             <div id="notorietySection" className="section-credentials" style={{textAlign: "center"}}>
                 <h1>Notoriety</h1>
-                // Content here...
+                <div className="buttonContainer">
+                <p style={{opacity: "0.77"}}>This section unveils an array of awards, honors, media features, and notable acknowledgments that exemplify my commitment to excellence, growth, and societal contributions.</p>
+                </div>
             </div>
         </div>
     );
