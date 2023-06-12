5cdb04563fb18c98eb690d5ba972e970d5313051
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-12 14:28:04 -0500

    Next section complete

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import Mac1 from "../animations/Mac1.json";
+import Create from "../animations/Create.json";
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: Create,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+};
 
 const About = () => {
     return(
@@ -34,7 +46,19 @@ const About = () => {
 
             {/* Creation Section */}
             <div id="creationSection" className="section-about">
-                // Content here...
+                <div className="creation-content" style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                    <div className="creation-text">
+                        <p>The act of creation lies at the heart of all my endeavors, serving as the core that underpins each project I undertake. My creativity extends across diverse platforms and mediums, encompassing ventures such as creating and editing content for YouTube, solving complex problems through the development of innovative software solutions, and engaging in the artistic interweaving of sounds and instruments to create new pieces of music. The energy of my creative spirit permeates every aspect of my work, casting an aura that fuels my passion and motivates me to push the boundaries of what is conventionally possible.</p>
+                    </div>
+
+                    <div id="creation-animation">
+                        <Lottie animationData={Create} style={{ height: '40vh', width: '40vw' }} loop autoplay />
+                    </div>
+
+                    <div className="creation-text">
+                        <p>As the pages of my life's book continue to turn, the ember of passion for pioneering innovative business concepts within me is only growing stronger and brighter. With an observant eye I identify unique opportunities with out-of-the-box thinking, I am always prepared to challenge the status quo. My creativity is not confined within traditional parameters but rather expands into the realm of entrepreneurial endeavors. This allows me to integrate a unique blend of imagination, strategy, and pragmatism into my ventures, transforming ideas into tangible realities that hold the potential to disrupt markets and pioneer novel paradigms.</p>
+                    </div>
+                </div>
             </div>
 
             {/* Personal Interests Section */}
