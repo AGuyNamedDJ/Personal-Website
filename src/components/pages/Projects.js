6d786afd5c9d16c27353eecf4da3f5481f64cc5d
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-06-13 15:59:35 -0500

    Updating page infrastructure

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,47 +1,47 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
     return(
-        <div id="projectsPage" className="page">
+        <div id="Page" className="page">
             {/* Page Title */}
             <div id="titleSection-template">
                 <h1>Projects</h1>
             </div>
 
-            {/* Coding Section */}
-            <div id="CodingSection">
-                <h1>Coding</h1>
-                <div className="buttonContainer">
-                    <a>
-                        sdfgfff
-                    </a>
+            <div id="projectsPage">
+                {/* Coding Section */}
+                <div className="project-section" id="codingSection">
+                    <h2>Coding. Beyond.</h2>
+                    <p>Witness the alchemy of ideas transformed into digital reality. This is the culmination of my innovative journey in software development, a curated showcase of technological ingenuity.</p>
+                    {/* Link to Project */}
+                    <Link to="/projects/websites" className="learn-more">Learn More</Link>
+                </div>
+
+                {/* Trading Section */}
+                <div className="project-section" id="tradingSection">
+                    <h2>Trading. Beyond boundaries.</h2>
+                    <p>Embark on a journey into the nuanced world of trading. Explore my strategic approaches, witness calculated risks in action, and gain insights from my real-time market maneuvers. Below you'll find the most recent video from my YouTube channel, providing a tangible window into my trading practices and thought processes.</p>
+
+                    {/* Video of Trading  */}
+                    <iframe 
+                        id="tradingVideo"
+                        src="https://www.youtube.com/embed/-5_NiRTS2nE?autoplay=1&mute=1" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen>
+                    </iframe>
+                </div>
+
+                {/* Writing Section */}
+                <div className="project-section" id="writingSection">
+                    <h2>Writing. Beyond boundaries imagined.</h2>
+                    <p>Dive into a realm where thoughts and emotions take form, a display of my creative endeavors in writing. Each piece is a part of me, unfettered and expressed freely.</p>
+                    {/* Link to Writing Page */}
+                    <Link to="/writing" className="learn-more">Learn More</Link>
                 </div>
             </div>
-
-            {/* Derivatives Section */}
-            <div id="DerivativesSection">
-                <h1>Derivatives</h1>
-                <div className="buttonContainer">
-                    <a>
-                        sdfgfff
-                    </a>
-                </div>
-            </div>
-
-            {/* Writings Section */}
-            <div id="WritingsSection">
-                <h1>Writings</h1>
-                <div className="buttonContainer">
-                    <a>
-                        sdfgfff
-                    </a>
-                </div>
-            </div>
-
-
-
         </div>
     )
 };
-
 export default Projects;
