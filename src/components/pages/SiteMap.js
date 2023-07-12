4118e2a17b62e9f3662c28dcbe1dd436f2765d40
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-07-12 11:23:41 -0500

    Updatomg UX

--- src/components/pages/SiteMap.js
+++ src/components/pages/SiteMap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SiteMap = () => {
     return(
@@ -15,25 +16,71 @@ const SiteMap = () => {
                 </div>
                 <div className="site-map-container">
                     <div className="site-map-1">
-                        <p>random data</p>
-                        <p>random data</p>
-                        <p>random data</p>
+                        <p>DJR</p>
+                        <p>Read My Story</p>
+                        <p>Personal Interest</p>
                     </div>
                     <div className="site-map-2">
-                        <p>random data</p>
-                        <p>random data</p>
-                        <p>random data</p>
-                        <p>random data</p>
-                        <p>random data</p>
+
                     </div>
                     <div className="site-map-3">
-                        <p>random data</p>
-                        <p>random data</p>
-                        <p>random data</p>
+
                     </div>
                 </div>
             </div>
+
             {/* Projects */}
+            <div id="site-map-block">
+                <div className="site-map-title">
+                    <h2>Projects</h2>
+                </div>
+                <div className="site-map-container">
+                    <div className="site-map-1">
+                        <div className="site-map-subtitle">Software Development</div>
+                        <p>Derivative/Hedge Trading</p>
+                        <p>Literary Works</p>
+                        <br></br>
+                        <div className="site-map-subtitle">Websites</div>
+                        <p>Personal Website</p>
+                        <p>Trading View Strategies</p>
+                        <p>Health Hive</p>
+                        <p>Car Haven</p>
+                        <p>Dal-Recipe</p>
+                        <p>The Sweet Spot</p>
+                        <p>Fitness Trackr</p>
+                        <p>Juicebox</p>
+                        <p>Stranger's Things</p>
+                        <p>Hacker's News</p>
+                        <p>Puppy Bowl</p>
+                        <p>Connect 4</p>
+                        <p>Reak Estate Clicker</p>
+                    </div>
+                    <div className="site-map-2">
+                        <div className="site-map-subtitle">Books</div>
+                        <p>Circuit Breakers: One</p>
+                        <br></br>
+                        <div className="site-map-subtitle">YouTube</div>
+                        <p>Behind The Lens</p>
+                        <p>God's Grace</p>
+                        <p>Life Upgraded</p>
+                        <p>Music Spaces</p>
+                        <p>Reflections and Revelations</p>
+                        <p>The Dreamer's Desk</p>
+                        <p>Tech Savvy</p>
+                        <p>Trading Markets</p>
+                    </div>
+                    <div className="site-map-3">
+                        <div className="site-map-subtitle">Journals</div>
+                        <p>Love's Purpose</p>
+                        <p>The Tunnel</p>
+                        <p>A Busy Week</p>
+                        <p>New Aged Branding</p>
+                        <p>Digital Harmony</p>
+                        <p>Evidence of Light</p>
+                    </div>
+                </div>
+            </div>
+
             {/* Ventures */}
             {/* Credentials */}
             {/* Connect */}
