8760dd4b2a14312ffb85821cf4c93de7eb49890f
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-07-12 10:26:06 -0500

    Updates

--- src/components/pages/SiteMap.js
+++ src/components/pages/SiteMap.js
@@ -2,8 +2,46 @@ import React from "react";
 
 const SiteMap = () => {
     return(
-        <div>
-            <p>Here is your default</p>
+        <div id="Page" className="page">
+            {/* Page Title */}
+            <div id="titleSection-template">
+                <h1>Site Map</h1>
+            </div>
+
+            {/* About */}
+            <div id="site-map-block">
+                <div className="site-map-title">
+                    <h2>About</h2>
+                </div>
+                <div className="site-map-columns">
+                    <div className="site-map-1">
+                        <p>random data</p>
+                        <p>random data</p>
+                        <p>random data</p>
+                    </div>
+                    <div className="site-map-2">
+                        <p>random data</p>
+                        <p>random data</p>
+                        <p>random data</p>
+                        <p>random data</p>
+                        <p>random data</p>
+                    </div>
+                    <div className="site-map-3">
+                        <p>random data</p>
+                        <p>random data</p>
+                        <p>random data</p>
+                    </div>
+                </div>
+            </div>
+            {/* Projects */}
+            {/* Ventures */}
+            {/* Credentials */}
+            {/* Connect */}
+
+
+
+
+
         </div>
     )
 };
